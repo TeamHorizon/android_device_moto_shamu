6011a23e25efb2f27044b10a706ed52aff9c5597
iceandfire <arhamjamal@gmail.com>
2015-11-10 01:52:26 +0530

    Revert "shamu: Add hardware/cyanogen for LiveDisplay features"
    This reverts commit fa3d3d9297d045d3664944850afaa992437cbbab.
    
    Signed-off-by: iceandfire <arhamjamal@gmail.com>
    
    Conflicts:
    	init.shamu.rc

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -140,10 +140,7 @@ BOARD_HAL_STATIC_LIBRARIES := libdumpstate.shamu
 EXTENDED_FONT_FOOTPRINT := true
 
 # CMHW
-BOARD_USES_CYANOGEN_HARDWARE := true
-BOARD_HARDWARE_CLASS := \
-    hardware/cyanogen   \
-    device/moto/shamu/cmhw
+BOARD_HARDWARE_CLASS := device/moto/shamu/cmhw
 
 USE_CLANG_PLATFORM_BUILD := true
 
