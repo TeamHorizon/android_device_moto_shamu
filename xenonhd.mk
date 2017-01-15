80128c844cedfffaeee8919762713f5dbac72af7
iceandfire <arhamjamal@gmail.com>
2017-01-15 23:41:48 +0530

    Add ro.xenonhd.maintainer
    Change-Id: I7c7bdf2982eb9bd267b89eaf884d017dfbad886e
    Signed-off-by: iceandfire <arhamjamal@gmail.com>

--- xenonhd.mk
+++ xenonhd.mk
@@ -10,6 +10,8 @@ $(call inherit-product, device/moto/shamu/aosp_shamu.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/moto/shamu/overlay-cm
 
+PRODUCT_PROPERTY_OVERRIDES += ro.xenonhd.maintainer=iceandfire
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := xenonhd_shamu
 PRODUCT_BRAND := google
