10e55ea6b13d4c250eca026bd98af5b21cf0b191
iceandfire <arhamjamal@gmail.com>
2017-04-17 13:10:07 +0530

    Shamu: include KernelAdiutor and AdAway
    Change-Id: If0b0f2235f8c45bf07d9e3c52a130dc8daefaffe
    Signed-off-by: iceandfire <arhamjamal@gmail.com>

--- xenonhd.mk
+++ xenonhd.mk
@@ -19,6 +19,10 @@ PRODUCT_MODEL := Nexus 6
 
 TARGET_VENDOR := moto
 
+PRODUCT_PACKAGES += \
+    Adaway \
+    KernelAdiutor
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=shamu \
     BUILD_FINGERPRINT=google/shamu/shamu:7.1.1/N6F26Y/3821691:user/release-keys \
